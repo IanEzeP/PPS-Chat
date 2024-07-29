c370c50e8fe587869200766c434ae0cdb5b28a7c
IanEzeP <pereyra.ian00@gmail.com>
2024-07-29 11:22:50 -0300

    Login personalizado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'pps.iep.chat',
-  appName: 'Conversando en el aula',
+  appName: 'Conversando aula',
   webDir: 'www'
 };
 
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -40,12 +40,14 @@ export class LoginPage implements OnInit, OnDestroy {
     this.subsDatabase = this.data.getCollectionObservable('usuarios').subscribe((next: any) => {
       let result: Array<any> = next;
       this.arrayFirebase = [];
-      this.arrayTestUsers = [];
+      //this.arrayTestUsers = [];
 
       result.forEach((obj: any) => {
         this.arrayFirebase.push(new User(obj.id, obj.correo, obj.clave, obj.perfil, obj.sexo, obj.nombre));
+        
+        this.cargaFin = true;
       });
-      
+      /*
       this.arrayFirebase.forEach(user => {
         if (user.id == 1 || user.id == 2 || user.id == 3) {
           this.arrayTestUsers.push(user);
@@ -53,7 +55,7 @@ export class LoginPage implements OnInit, OnDestroy {
 
         this.cargaFin = true;
         console.log("Carga Fin");
-      });
+      });*/
     });
   }
 
@@ -110,9 +112,9 @@ export class LoginPage implements OnInit, OnDestroy {
     }
   }
 
-  onQuickUser(user: any) {
-    this.formLog.controls['email'].setValue(user.target.value.correo);
-    this.formLog.controls['password'].setValue(user.target.value.clave);
+  fastLogin(email: string, clave: number) {
+    this.formLog.controls['email'].setValue(email);
+    this.formLog.controls['password'].setValue(clave);
   }
 
   cleanInputs() {
